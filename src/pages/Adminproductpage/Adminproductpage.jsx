14b795a2b8d28107f3ac1b73494bc0b9c21b9cf0
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-21 17:00:34 +0530

    Error page is done

--- src/pages/Adminproductpage/Adminproductpage.jsx
+++ src/pages/Adminproductpage/Adminproductpage.jsx
@@ -12,3 +12,4 @@ export default function Adminproductpage(){
         </div>
     )
 }
+
--- src/pages/Errorpage.jsx
+++ src/pages/Errorpage.jsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
 
 export  default function Errorpage(){
     return(
-        <div className="h-screen w-[600px] bg-gray-300 flex flex-col">
+        <div className="flex h-screen">
+        <div className=" w-[45%] bg-gray-300 flex flex-col items-center justify-center">
             <div>
-                <h1 className="mt-52 text-4xl text-cyan-800 bg-amber-500 " >OOPS!</h1>
+                <h1 className="mt-52 text-4xl text-cyan-800  " >OOPS!</h1>
             </div>
             
-            <h2 className="mt-5 font-bold text-6xl">Page not found</h2>
+            <h2 className="mt-5 font-bold text-6xl from-neutral-900">Page not found</h2>
             <h3 className="mt-6 ">Sorry,we couldn't find your are looking for.</h3>
 
         <div className="mt-10 ">
-            <Link to="/*">Back to home</Link>
-        </div>
-    
-
+            <Link className="flex flex-row items-center  text-zinc-600 hover:text-slate-900" to="/*" ><MdKeyboardDoubleArrowLeft className="mr-2"/>Back to home</Link>
+        
         </div>
         
-    )
+        <div className="bg-red-300 mt-52 py-8 px-28 w-[100%] item center justify-center h-screen" >Hello</div>
+       </div>
+
+       <div className="bg-orange-200  h-screen w-[55%]">
+        <img src="https://www.istockphoto.com/photo/warning-attention-symbol-with-exclamation-mark-in-the-triangle-on-the-abstract-human-gm2148676029-569861452?utm_source=pixabay&utm_medium=affiliate&utm_campaign=SRP_image_sponsored&utm_content=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Ferror%2F&utm_term=error"
+          alt="This is a picture"
+          className="max-h-full max-w-full object-contain"/>
+       </div>
+        
+     </div>
+        
+    );
 
 
 }
