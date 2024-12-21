095510ce845dc2d8509ac9e5fa07485733664f35
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2024-12-22 00:35:01 +0530

    contactus logo was created.

--- src/pages/Home/contactus.jsx
+++ src/pages/Home/contactus.jsx
@@ -6,7 +6,7 @@ export default function Contactus(){
         <div className="bg-white p-8 rounded-lg shadow-lg w-[60%] h-96 flex flex-col">
             <h1>ContactUs</h1>
           <div className="bg-emerald-600 h-72 w-72 items-start justify-start mt-4"> 
-             <img  src="https://www.canva.com/icons/MAGTC9jfut4-click-button-contact-us-button-icon/"
+             <img  src="/contactus.png"
             alt="This is canva picture."
             className="max-h-full max-w-full object-contain"/>
             </div>
