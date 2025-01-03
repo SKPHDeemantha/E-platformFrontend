88b5565afa2df86c8869d70f59fa8daf527cc091
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-03 20:31:23 +0530

    Improve media upload functionality: add success and error notifications, refactor upload logic, and remove sensitive key file

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react"
 import UploadMeadiaToSupabase from "../utils/MediaUpload";
+import toast from "react-hot-toast";
 
 export default function FileUploardTest(){
 
-    const [file,SetFile] =useState(null)
+    const [file,SetFile] =useState(null);
+
 
  async  function handleUpload(){
           UploadMeadiaToSupabase(file).then((url)=>{
             console.log(url)
+            toast.success("Image is uploaded successfully.");
           }).catch((err)=>{
-            console.log(err)
+             console.log("oops! coudn't upload image",err)
+             toast.error("Please try again");
           })
  }
     return(
--- src/utils/MediaUpload.js
+++ src/utils/MediaUpload.js
@@ -1,43 +1,36 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const key =`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmF
-            zZSIsInJlZiI6Inh2dXhzd3Z4ZHN4emZqdHNkb3JuIiwicm9sZSI6ImFu
-            b24iLCJpYXQiOjE3MzUzOTQ2NzIsImV4cCI6MjA1MDk3MDY3Mn0.WBdf
-            4BPSGdNVmg2rP6fhwd1tmIoD1qZ8vkRJLX4MLq8`;
+const key =`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2dXhzd3Z4ZHN4emZqdHNkb3JuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzOTQ2NzIsImV4cCI6MjA1MDk3MDY3Mn0.WBdf4BPSGdNVmg2rP6fhwd1tmIoD1qZ8vkRJLX4MLq8`;
+
 
  const url= "https://xvuxswvxdsxzfjtsdorn.supabase.co"
 
-export default function UploadMeadiaToSupabase(file){
-      
-    return new Promise((resolve, reject) => {
-        if(file==null){
-          reject("file not added");
-        }
+ export default function uploadMediaToSupabase(file) {
+  return new Promise((resolve, reject) => {
+    if (file == null) {
+      reject("File not added");
+    }
+    let fileName = file.name;
+    const extension = fileName.split(".")[fileName.split(".").length - 1];
 
-       const fileName =file.name
-        const extension = fileName.split(".")[fileName.split(".").length-1];
-        
-        const supabase =createClient(url,key);
+    const supabase = createClient(url, key);
 
-         const timestamp =new Date().getTime()
-         console.log(timestamp +"." + extension)
+    const timestamp = new Date().getTime();
 
+    fileName = timestamp +file.name+ "." + extension;
 
-         supabase.storage.from("images").upload(file.name,file,
-         {
-            cashControl : "3600",
-            upsert : false
-         }).then(()=>{
-            const publicUrl =supabase.storage.from("images").getPublicUrl(file.name).data.publicUrl;
-            resolve(publicUrl);
-         }).catch((err)=>{
-            reject(err);
-         });
+    supabase.storage.from("images").upload(fileName, file, {
+      cacheControl: "3600",
+      upsert: false,
+    }).then(()=>{
+      const publicUrl = supabase.storage.from("images").getPublicUrl(fileName).data.publicUrl;
+      resolve(publicUrl);
+    }).catch((err)=>{
+      reject(err);
     });
+  });
 }
-
-
 
 
 // if(file==null){
