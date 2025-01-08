4603b087f2ff2e80ad5e23e0e48cf6bb8133db6f
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-08 17:36:15 +0530

    Add Admin Dashboard component and update AdminHomepage for navigation

---
+++ src/pages/Admin/AdminDashBord.jsx
@@ -0,0 +1,11 @@
+export default function Dashboard(){
+    return (
+        <div className="bg-white w-screen h-screen flex ">
+          <div className="bg-slate-300 shadow-lg w-52 h-52 ">
+    
+          </div>
+
+          <div></div>
+        </div>
+    )
+}
--- src/pages/AdminHomepage.jsx
+++ src/pages/AdminHomepage.jsx
@@ -7,13 +7,14 @@ import Adminproductpage from "./Admin/Adminproductpage";
 import { Routes, Route } from "react-router-dom";
 import AddproductForm from "./Admin/AdminproductForm";
 import Admincustomerpage from "./Admin/AdmincustomerPage";
+import Dashboard from "./Admin/AdminDashBord";
 
 
 export default function AdminHomepage(){
     return(
         <div className="bg-cyan-600  w-full h-screen flex">
 
-            <div className="bg-zinc-300 w-[20%] h-screen flex flex-col items-center text-justify ">
+            <div className="bg-zinc-300 w-[20%] h-screen flex flex-col items-center text-justify rounded-lg ">
                 <Link className="flex flex-row items-center text-zinc-600 hover:text-slate-900 mb-2 mr-4" to="/admin/dashboard  "><VscGraph/>Dashboard</Link>
                 <Link className="flex flex-row items-center text-zinc-600 hover:text-slate-900 mb-2" to="/admin/products "><FaJediOrder/>Products</Link>
                 <Link className="flex flex-row items-center text-zinc-600 hover:text-slate-900 mb-2"  to="/admin/orders "><MdProductionQuantityLimits/>Order</Link>
@@ -22,7 +23,7 @@ export default function AdminHomepage(){
 
     <div className="bg-indigo-950 w-[80%] h-screen" >
     <Routes path="/*">
-          <Route path="/dashboard" element={<h1 className="text-sky-400">Dashboard</h1>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/products" element={< Adminproductpage/>} />
           <Route path="/admin/products/addproduct" element={<AddproductForm/>} />
           <Route path="/orders" element={<h1 className="text-sky-400" >Orders</h1>} />
