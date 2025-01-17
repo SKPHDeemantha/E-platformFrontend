8d4c292fc591968b25dd9e26d493107a3bab6e98
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-17 23:16:47 +0530

    Refactor EditProductForm; update image handling and improve upload functionality

--- src/pages/Admin/EditProductForm.jsx
+++ src/pages/Admin/EditProductForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
-import UploadMeadiaToSupabase from "../../utils/MediaUpload";
+import uploadMediaToSupabase from "../../utils/MediaUpload";
 
 export default function EditProductForm() {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ export default function EditProductForm() {
   const [ProductId, setProductId] = useState(product.ProductId);
   const [productName, setProductName] = useState(product.productName);
   const [alternativeNames, setAlternativeNames] = useState(product.altNames.join(","));
-  const [images, setImages] = useState([]);
+  const [imageFile, setImageFile] = useState([]);
   const [price, setPrice] = useState(product.price);
   const [lastPrice, setLastPrice] = useState(product.lastPrice);
   const [stock, setStock] = useState(product.stock);
@@ -25,18 +25,26 @@ export default function EditProductForm() {
 
   async function handleSubmit() {
     const altNames = alternativeNames.split(",");
-
+     
+    const promisesArray = [];
+    let imgUrls = product.images;
+    if (imageFile.length > 0) {
+      for (let i = 0; i < imageFile.length; i++) {
+        promisesArray[i] = uploadMediaToSupabase(imageFile[i]);
+      }
+      imgUrls = await Promise.all(promisesArray);
+    }
     const productData = {
       ProductId,
       productName,
       altNames,
-      images,
+      images: imgUrls,
       price: parseFloat(price),
       lastPrice: parseFloat(lastPrice),
       stock,
       description,
     };
-  console.log(productData);
+  
     const token = localStorage.getItem("token");
 
     try {
@@ -56,16 +64,6 @@ export default function EditProductForm() {
     }
   }
 
-  async  function handleUpload(){
-    UploadMeadiaToSupabase(images).then((url)=>{
-      console.log(url)
-      toast.success("Image is uploaded successfully.");
-    }).catch((err)=>{
-       console.log("oops! coudn't upload image",err)
-       toast.error("Please try again");
-    })
-}
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 p-6">
       <h1 className="text-4xl font-bold text-white mb-8">Edit Product</h1>
@@ -79,7 +77,7 @@ export default function EditProductForm() {
           value={ProductId}
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
         />
-         <label>Product Name</label>
+        <label>Product Name</label>
         <input
           type="text"
           id="product-name"
@@ -88,27 +86,25 @@ export default function EditProductForm() {
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
           placeholder="Enter Product Name"
         />
-         <label>Alternative Names</label>
+        <label>Alternative Names</label>
         <input
           type="text"
           id="alternative-names"
           value={alternativeNames}
           onChange={(e) => setAlternativeNames(e.target.value)}
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-          placeholder="Enter Alternames"
+          placeholder="Enter Alternative Names"
         />
-         <label>Images</label>
+        <label>Images</label>
         <input
-          type="File"
-          id="image-urls"   
-          onChange={(e)=>{setImages(e.target.files[0])}}
+          type="file"
+          id="image-urls"
+          onChange={(e) => setImageFile(e.target.files)}
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-          placeholder="upload Images"
-          onClick={handleUpload}
-          value={images}
+          placeholder="Upload Images"
         />
-         <label>Price</label>
-         <input
+        <label>Price</label>
+        <input
           type="text"
           id="price"
           value={price}
@@ -116,26 +112,26 @@ export default function EditProductForm() {
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
           placeholder="Enter Price"
         />
-         <label>Last Price</label>
-         <input
+        <label>Last Price</label>
+        <input
           type="text"
           id="last-price"
           value={lastPrice}
           onChange={(e) => setLastPrice(e.target.value)}
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-          placeholder="Enter Last price"
+          placeholder="Enter Last Price"
         />
-         <label>Stock</label>
-         <input
+        <label>Stock</label>
+        <input
           type="text"
           id="stock"
           value={stock}
           onChange={(e) => setStock(e.target.value)}
           className="w-full p-3 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600"
-          placeholder="Enter stock"
+          placeholder="Enter Stock"
         />
-         <label>Description</label>
-         <input
+        <label>Description</label>
+        <input
           type="text"
           id="description"
           value={description}
