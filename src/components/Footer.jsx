763f31bce423549910e896dc3be00b584c38ed6c
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-28 07:00:20 +0530

    Refactor Footer component to replace Link with anchor tags for social media; update styling and accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import { FaFacebookSquare } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
@@ -29,10 +28,11 @@ export default function Footer() {
                     <div>
                         <h3 className="text-xl font-semibold mb-4">Follow Us</h3>
                         <div className="flex space-x-4">
-                            <Link className="hover:text-gray-300 h-10 w-6" fill="currentColor" viewBox="0 0 24 24 " onClick={'https://www.facebook.com/'}>
-                            <FaFacebookSquare />
-                            </Link>
-                    
+                            <a href="#" className="hover:text-gray-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M24 4.557a10.05 10.05 0 01-2.828.775 4.933 4.933 0 002.165-2.723c-.943.559-1.987.966-3.1 1.184A4.92 4.92 0 0016.616 3c-2.724 0-4.932 2.209-4.932 4.932 0 .387.045.764.127 1.128C7.728 8.89 4.1 6.657 1.671 3.149a4.922 4.922 0 00-.667 2.478c0 1.71.869 3.217 2.188 4.096a4.902 4.902 0 01-2.23-.616v.062c0 2.385 1.697 4.375 3.946 4.83a4.933 4.933 0 01-2.224.084c.626 1.956 2.444 3.381 4.604 3.423a9.867 9.867 0 01-6.11 2.104c-.398 0-.79-.023-1.175-.068a13.945 13.945 0 007.557 2.209c9.054 0 14.002-7.506 14.002-14.002 0-.213-.005-.425-.014-.637A10.025 10.025 0 0024 4.557z" />
+                                </svg>
+                            </a>
                             <a href="#" className="hover:text-gray-300">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
                                     <path d="M22.675 0h-21.35c-.733 0-1.325.592-1.325 1.325v21.351c0 .732.592 1.324 1.325 1.324h11.497v-9.294h-3.124v-3.622h3.124v-2.672c0-3.1 1.891-4.788 4.65-4.788 1.325 0 2.464.099 2.793.143v3.238h-1.918c-1.504 0-1.795.715-1.795 1.763v2.316h3.59l-.467 3.622h-3.123v9.294h6.127c.732 0 1.324-.592 1.324-1.324v-21.35c0-.733-.592-1.325-1.325-1.325z" />
