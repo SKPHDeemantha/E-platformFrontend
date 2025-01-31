cde0061dbbd7d150549a2c8558b39b68fa75a7dd
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-31 21:42:24 +0530

    Refactor Header component for improved styling and structure; enhance link hover effects and adjust layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,60 +19,46 @@ export default function Header() {
         <img
           src="/company logo.jpg"
           alt="Company Logo"
-          className="cursor-pointer rounded-full w-16 h-16 md:w-20 md:h-20 absolute left-4"
-        />
+          className="cursor-pointer rounded-full w-16 h-16 md:w-20 md:h-20 absolute left-8"/>
 
-{/*     
-        <div className="flex items-center space-x-2 w-full max-w-[70%] lg:max-w-[50%] md:ml-20">
-          <input
-            type="text"
-            placeholder="Search for products"
-            className="rounded-lg p-2 w-full md:w-96 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent"
-          />
-          <Link className="p-2 text-xl text-white hover:text-pink-900">
-            <ImSearch />
-          </Link>
-        </div> */}
-
-       
         <FiAlignJustify
           onClick={() => setIsSliderOpen(true)}
-          className="text-3xl cursor-pointer text-white lg:hidden absolute right-20"
-        />
+          className="text-3xl cursor-pointer text-white lg:hidden absolute right-20"/>
 
+
+
+      
+        <nav className="hidden lg:flex space-x-8 items-center justify-center absolute right-44">
 
         <div className="relative hidden lg:flex items-center ">
           <Link
-            className="p-2 text-xl text-white hover:text-pink-900 hover:shadow-xl hover:outline flex items-center hover:scale-105 transition-transform hover:rounded-lg"
+            className="p-2 text-xl text-white shadow-xl hover:text-pink-900 hover:shadow-xl hover:outline flex items-center hover:scale-105 transition-transform hover:rounded-lg"
             to="/cart" >
             <span className=" text-white hover:text-pink-900  text-transparent"> View Cart</span>
             <GiShoppingCart className="ml-1" />
           </Link>
         </div>
-
-      
-        <nav className="hidden lg:flex space-x-8 items-center">
           <Link
             to="/"
-            className="text-white font-bold p-2 bg-opacity-30 text-xl hover:shadow-xl hover:outline hover:border-b border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
+            className="text-white font-bold shadow-xl p-2 bg-opacity-30 text-xl hover:shadow-xl hover:outline hover:border-b border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
           >
             Home
           </Link>
           <Link
             to="/products"
-            className="text-white font-bold p-2 text-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
+            className="text-white font-bold p-2 shadow-xl text-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
           >
             Products
           </Link>
           <Link
             to="/contactus"
-            className="text-white p-2 font-bold text-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
+            className="text-white p-2 font-bold shadow-xl text-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
           >
             Contact Us
           </Link>
           <Link
             to="/signup"
-            className="text-white p-2 font-bold text-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
+            className="text-white p-2 font-bold text-xl shadow-xl hover:border-b hover:shadow-xl hover:outline border-primary hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
           >
             Sign Up
           </Link>
@@ -81,12 +67,12 @@ export default function Header() {
         {/* Cart for Mobile Screens */}
         <div className="relative lg:hidden">
           <Link
-            className="p-2 text-2xl text-white hover:shadow-xl hover:outline hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
+            className="p-2 text-2xl text-white shadow-xl hover:shadow-xl hover:outline hover:text-pink-900 hover:scale-105 transition-transform hover:rounded-lg"
             to="/cart"
           >
             <GiShoppingCart />
           </Link>
-          <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1">
+          <span className="absolute top-0 right-0 shadow-xl bg-red-500 text-white text-xs rounded-full px-1">
       
           </span>
         </div>
