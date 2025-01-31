0d916756183ff8e1ffb5121dd080a54d5b9f8d51
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-01-31 23:47:27 +0530

    Implement search functionality in Productpage; add input for product filtering and update loading status

--- src/pages/Home/Productpage.jsx
+++ src/pages/Home/Productpage.jsx
@@ -9,6 +9,7 @@ import Footer from '../../components/Footer';
 export default function Productpage(){
   const [products,setProducts] =useState([]);
   const [loadingstatus,setLoadingstatus] =useState("loading");
+  const [query, setQuery] = useState("");
 
   useEffect(
     ()=>{
@@ -30,19 +31,50 @@ export default function Productpage(){
     }
   ,[])
 
+  function search(e) {
+    const query = e.target.value;
+    setQuery(query);
+    setLoadingstatus("loading");
+    if (query == "") {
+      axios
+        .get(`http://localhost:3000/api/products`)
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data);
+          setLoadingstatus("loaded");
+        })
+        .catch((err) => toast.error("Error loading products"));
+    }else{
+      axios
+        .get(`http://localhost:3000/api/products/search/`+query)
+        .then((res) => {
+          console.log(res.data);
+          setProducts(res.data);
+          setLoadingstatus("loaded");
+        })
+        .catch((err) => toast.error("Error loading products"));
+    }
+  }
+
   return(
     <div className="flex flex-wrap justify-center items-center bg-[#f1f1f1] overflow-y-scroll w-full h-full">
       <Header/>
-      <div className="flex items-center space-x-2 w-full max-w-[70%] lg:max-w-[50%] md:ml-20">
-          <input
-            type="text"
-            placeholder="Search for products"
-            className="rounded-lg p-2 w-full md:w-96 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-accent"
-          />
-          <Link className="p-2 text-xl text-white hover:text-pink-900">
-            <ImSearch />
-          </Link>
-        </div>
+      <div className="flex items-center space-x-2 w-full max-w-[70%] lg:max-w-[50%] md:ml-20 absolute top-28 bg-white rounded-full shadow-md p-2">
+  {/* Search Input */}
+  <div className=' absolute justify-center'>
+  <input
+    type="text"
+    onChange={search}
+    value={query}
+    placeholder="Search for products..."
+    className="w-full md:w-96 p-3 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-pink-500 transition-all duration-300 z-10"
+  />
+   </div>
+  <Link className="bg-pink-500 text-white p-3 rounded-full shadow-md hover:bg-pink-700 transition-all duration-300">
+    <ImSearch className="text-xl" />
+  </Link>
+</div >
+
         {
             products.map(
              (product)=>
@@ -50,6 +82,7 @@ export default function Productpage(){
           
              )
            }
+         
            <Footer/>
       </div>
   )
