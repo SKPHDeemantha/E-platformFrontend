81c2edec1c0cc161726e62c9cafa605c4076566a
SKPHDeemantha <22cse0348@ms.sab.ac.lk>
2025-02-05 00:55:42 +0530

    Enhance ProductOverview with star rating and review modal; improve layout and counter functionality

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
+
 export default function StarRating() {
   const stars = Array(5).fill(0);
   const [currentValue, setCurrentValue] = useState(0);
@@ -18,8 +19,10 @@ export default function StarRating() {
     setHoverValue(undefined);
   };
 
+
   return (
-    <div className="flex flex-col items-center p-4 bg-slate-400 rounded-md shadow-lg">
+    <div className="flex flex-col items-center p-4 bg-slate-200 rounded-md shadow-2xl ">
+          
       <div className="flex space-x-2">
         {stars.map((_, index) => {
           const starValue = index + 1;
@@ -30,7 +33,7 @@ export default function StarRating() {
               className={`cursor-pointer transition-colors duration-300 ${
                 (hoverValue || currentValue) >= starValue
                   ? "text-yellow-400"
-                  : "text-gray-300"
+                  : "text-gray-500"
               }`}
               onClick={() => handleClick(starValue)}
               onMouseOver={() => handleMouseOver(starValue)}
